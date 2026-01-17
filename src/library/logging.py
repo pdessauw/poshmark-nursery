a1bac938ab396f31bc90b9efab3ace604b282709
pdessauw <pdessauw@gmail.com>
2026-01-17 14:17:58 -0500

    feat: add logging capabilities

---
+++ src/library/logging.py
@@ -0,0 +1,39 @@
+"""Logging configuration"""
+
+import logging.config
+
+from library.settings import BASE_DIR, DEBUG
+
+LOGGER_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s][%(levelname)s] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "default",
+            "filename": f"{BASE_DIR}/debug.log",
+            "maxBytes": 25000,
+            "backupCount": 3,
+        },
+    },
+    "loggers": {
+        "app": {
+            "level": "DEBUG" if DEBUG else "INFO",
+            "handlers": ["console", "file"],
+            "propagate": "no",
+        }
+    },
+}
+
+logging.config.dictConfig(LOGGER_CONFIG)
+LOGGER = logging.getLogger("app")
---
+++ src/library/settings.py
@@ -0,0 +1,5 @@
+from os.path import dirname, abspath
+
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
+
+DEBUG = False
